0aafa186207f7a0774779a43d5b15dce0cafd2f6
ItzWiz <wizkid9876@gmail.com>
2025-03-18 18:33:58 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+
+module.exports = {
+    darkMode: 'class', // crucial for manual toggles
+    content: [
+      './index.html',  // or wherever your HTML files are
+      // './src/**/*.{html,js}', etc.
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
